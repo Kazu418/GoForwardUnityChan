81cf7f7e4f44dbc26ef33b923f23d07b3a8e88c4
西村和人 <kazuzon.0418@gmail.com>
2020-03-25 18:33:39 +0900

    GoForwardUnityChan

---
+++ Assets/CubeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    private float speed = -12f;
+    private float deadLine = -10f;
+    public AudioClip Block1;
+    private AudioSource audiosource;
+    
+    // Use this for initialization
+    void Start () {
+        audiosource=gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+        if (transform.position.x < deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Cube"||collision.gameObject.tag == "ground")
+        {
+            audiosource.PlayOneShot(Block1);
+        }
+    }
+    
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+    // Use this for initialization
+    private float groundLevel = -3.0f;
+    Animator animator;
+    Rigidbody2D rigidbody2d;
+    private float dump = 0.8f;
+    float jumpVelocity = 20;
+    private float deadLine = -9;
+    void Start () {
+        this.animator = GetComponent<Animator>();
+        this.rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.animator.SetFloat("Horizontal",1);
+        bool isGround = (this.transform.position.y > this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+        GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+        if (Input.GetMouseButtonDown(0) && isGround)
+        {
+            this.rigidbody2d.velocity = new Vector2(0, this.jumpVelocity);
+        }
+        if (Input.GetMouseButton(0) == false)
+        {
+            if(rigidbody2d.velocity.y > 0)
+            {
+                this.rigidbody2d.velocity *= this.dump;
+            }
+            
+        }
+        if(transform.position.x < this.deadLine)
+        {
+            GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+            Destroy(gameObject);
+        }
+    }
+}
